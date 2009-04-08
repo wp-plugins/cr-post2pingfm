35723acfe42fe276db28a661d45d584ea510db02
silentwind <silentwind@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-08 04:47:00 +0000

    initial commit release
    git-svn-id: https://plugins.svn.wordpress.org/cr-post2pingfm/tags/0.2@108628 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cr-post-2-pingfm.php
@@ -0,0 +1,104 @@
+<?php
+/*
+Plugin Name: CR Post to Ping.fm
+Plugin URI: http://bayu.freelancer.web.id/
+Description: This plugin will submit your new post to ping.fm account.
+Version: 0.2
+Author: Arief Bayu Purwanto
+Author URI: http://bayu.freelancer.web.id/
+
+Version History:
+  0.1 - 2009-03-01
+    - Initial Development
+  0.2 - 2009-03-29
+    - Several bug-fixes
+    - Initial Release
+*/
+
+define('API_KEY', '41121eb3a56f921bc2957b2458d65bad');
+
+add_action('publish_post', 'cr_post_2_pingfm_submit_to_ping_fm');
+add_action('admin_menu', 'cr_post_2_pingfm_submit_config_admin');
+
+function cr_post_2_pingfm_submit_to_ping_fm($postId)
+{
+	//submitPingFM($postId);
+	if(!wp_is_post_revision($postId))
+	{
+		$this_post_submitted = get_option('cr_post_2_pingfm_submit_post_submitted_'.$postId, false);
+		if(!$this_post_submitted)
+		{
+			update_option('cr_post_2_pingfm_submit_post_submitted_'.$postId, true);
+			submitPingFM($postId);
+		}
+	}
+}
+
+
+function cr_post_2_pingfm_submit_config_admin()
+{
+	add_options_page('CR Post2Pingfm', 'CR Post2Pingfm', 8, __FILE__, 'cr_post_2_pingfm_submit_config_form');
+}
+
+function cr_post_2_pingfm_submit_config_form() {
+?><div class="wrap">
+<h2>CR Post2Pingfm</h2>
+
+<form method="post" action="options.php">
+<?php wp_nonce_field('update-options'); ?>
+
+<table class="form-table">
+
+<tr valign="top">
+<th scope="row">Ping.fm Application Key</th>
+<td><input type="text" name="cl_post_pingfm_api_key" value="<?php echo get_option('cl_post_pingfm_api_key'); ?>" /></td>
+</tr>
+
+<tr valign="top">
+<th scope="row">Ping Template</th>
+<td><input type="text" name="cl_post_pingfm_message_template" value="<?php echo get_option('cl_post_pingfm_message_template', 'I just post {title} on {url}'); ?>" /><br />
+Template Tags: <strong>{title}</strong> for <em>Post Title</em> and <strong>{url}</strong> for <em>Permalink URL</em> <br />
+eg: <em>I just post {title} on {url}</em></td>
+</tr>
+
+<!--<tr valign="top">
+<th scope="row">Ping mode</th>
+<td>
+	Allow submit new post to ping.fm only: <br />
+	<input type="radio" name="cl_post_pingfm_ping_mode" value="allow" <?php if(get_option('cl_post_pingfm_ping_mode') == "allow") { echo 'checked="checked"'; }?> />For this category<br />
+	<input type="radio" name="cl_post_pingfm_ping_mode" value="deny" <?php if(get_option('cl_post_pingfm_ping_mode') == "deny") { echo 'checked="checked"'; }?> />For category except this one<br />
+	Category list<input type="text" name="cl_post_pingfm_ping_mode_category" value="<?php echo get_option('cl_post_pingfm_ping_mode_category'); ?>" />(comma separated, eg: <em>1,23,55,10</em>)<br />
+</td>
+</tr>-->
+
+</table>
+
+<input type="hidden" name="action" value="update" />
+<input type="hidden" name="page_options" value="cl_post_pingfm_message_template,cl_post_pingfm_api_key,cl_post_pingfm_ping_mode_category,cl_post_pingfm_ping_mode" />
+
+<p class="submit">
+<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+</p>
+
+</form>
+</div><?php
+}
+
+function submitPingFM($postId)
+{
+	$post = get_post($postId);
+	include_once('PHPingFM.php');
+	$my_API_key = get_option('cl_post_pingfm_api_key');
+	$ping_template = get_option('cl_post_pingfm_message_template');
+	$pfm = new PHPingFM(API_KEY, $my_API_key, false);
+        $arrTemplate = array(
+            '{title}' => $post->post_title,
+            '{url}' => get_permalink($postId),
+        );
+
+        foreach($arrTemplate as $template => $template_data)
+        {
+            $ping_template = str_replace($template, $template_data, $ping_template);
+        }
+	$result = $pfm->post("status", $ping_template);
+}
