eedf659eb77605f116d4b14b10555d76fd2a4ce6
silentwind <silentwind@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-08 22:49:44 +0000

    create stable tag 0.4
    git-svn-id: https://plugins.svn.wordpress.org/cr-post2pingfm/tags/0.4@108929 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cr-post-2-pingfm.php
@@ -0,0 +1,156 @@
+<?php
+/*
+Plugin Name: CR Post to Ping.fm
+Plugin URI: http://bayu.freelancer.web.id/2009/03/31/wordpress-plugin-cr-post2pingfm/
+Description: This plugin will submit your new post to ping.fm account.
+Version: 0.2
+Author: Arief Bayu Purwanto
+Author URI: http://bayu.freelancer.web.id/
+
+Version History:
+  0.1 - 2009-03-01
+    - Initial Development
+  0.2 - 2009-03-29
+    - Several bug-fixes
+    - Initial Release
+*/
+
+define('API_KEY', '41121eb3a56f921bc2957b2458d65bad');
+
+add_action('publish_post', 'cr_post_2_pingfm_submit_to_ping_fm');
+add_action('admin_menu', 'cr_post_2_pingfm_submit_config_admin');
+
+function cr_post_2_pingfm_submit_to_ping_fm($postId)
+{
+    $continue = false;
+    $categories = array();
+    if(!wp_is_post_revision($postId))
+    {
+        $this_post_submitted = get_option('cr_post_2_pingfm_submit_post_submitted_'.$postId, false);
+        if(!$this_post_submitted)
+        {
+            $continue = true;
+        }
+
+        if($continue)
+        {
+            $theCats = get_the_category($postId);
+            foreach($theCats as $cats)
+            {
+                $categories[] = $cats->cat_ID;
+            }
+            $continue = isCategoriesAllowedToPing($postId, $categories);
+        }
+        if($continue)
+        {
+            update_option('cr_post_2_pingfm_submit_post_submitted_'.$postId, true);
+            submitPingFM($postId);
+        }
+    }
+}
+
+
+function cr_post_2_pingfm_submit_config_admin()
+{
+    add_options_page('CR Post2Pingfm', 'CR Post2Pingfm', 8, __FILE__, 'cr_post_2_pingfm_submit_config_form');
+}
+
+function cr_post_2_pingfm_submit_config_form() {
+?><div class="wrap">
+<h2>CR Post2Pingfm</h2>
+
+<form method="post" action="options.php">
+<?php wp_nonce_field('update-options'); ?>
+
+<table class="form-table">
+
+<tr valign="top">
+<th scope="row">Ping.fm Application Key</th>
+<td><input type="text" name="cl_post_pingfm_api_key" value="<?php echo get_option('cl_post_pingfm_api_key'); ?>" /></td>
+</tr>
+
+<tr valign="top">
+<th scope="row">Ping Template</th>
+<td><input type="text" name="cl_post_pingfm_message_template" value="<?php echo get_option('cl_post_pingfm_message_template', 'I just post {title} on {url}'); ?>" /><br />
+Template Tags: <strong>{title}</strong> for <em>Post Title</em> and <strong>{url}</strong> for <em>Permalink URL</em> <br />
+eg: <em>I just post {title} on {url}</em></td>
+</tr>
+
+<tr valign="top">
+<th scope="row">Ping mode</th>
+<td>
+    Allow submit new post to ping.fm: <br />
+    <input type="radio" name="cl_post_pingfm_ping_mode" value="all" <?php if(get_option('cl_post_pingfm_ping_mode') == "all") { echo 'checked="checked"'; }?> />For all categories (<em>all</em>)<br />
+    <input type="radio" name="cl_post_pingfm_ping_mode" value="allow" <?php if(get_option('cl_post_pingfm_ping_mode') == "allow") { echo 'checked="checked"'; }?> />For this categories (<em>allow</em>)<br />
+    <input type="radio" name="cl_post_pingfm_ping_mode" value="deny" <?php if(get_option('cl_post_pingfm_ping_mode') == "deny") { echo 'checked="checked"'; }?> />For categories except this one (<em>deny</em>)<br />
+    Category list<input type="text" name="cl_post_pingfm_ping_mode_category" value="<?php echo get_option('cl_post_pingfm_ping_mode_category'); ?>" />(comma separated, eg: <em>1,23,55,10</em>)<br />
+    Category list is ignored if mode <em>all</em> is selected.
+</td>
+</tr>
+
+</table>
+
+<input type="hidden" name="action" value="update" />
+<input type="hidden" name="page_options" value="cl_post_pingfm_message_template,cl_post_pingfm_api_key,cl_post_pingfm_ping_mode_category,cl_post_pingfm_ping_mode" />
+
+<p class="submit">
+<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+</p>
+
+</form>
+</div><?php
+}
+
+function submitPingFM($postId)
+{
+    $post = get_post($postId);
+    include_once('PHPingFM.php');
+    $my_API_key = get_option('cl_post_pingfm_api_key');
+    $ping_template = get_option('cl_post_pingfm_message_template');
+    $pfm = new PHPingFM(API_KEY, $my_API_key, false);
+    $arrTemplate = array(
+        '{title}' => $post->post_title,
+        '{url}' => get_permalink($postId),
+    );
+
+    foreach($arrTemplate as $template => $template_data)
+    {
+        $ping_template = str_replace($template, $template_data, $ping_template);
+    }
+    $result = $pfm->post("status", $ping_template);
+}
+
+function isCategoriesAllowedToPing($postId, $categories)
+{
+    $pingMode = get_option('cl_post_pingfm_ping_mode');
+    $pingCats = get_option('cl_post_pingfm_ping_mode_category');
+    $pingCats = array_map("trim", explode(",", $pingCats));
+    if(!is_array($pingCats)) $pingCats = array();
+
+    if("all" == $pingMode) return true;
+    
+    if("allow" == $pingMode)
+    {
+        foreach($categories as $cats)
+        {
+            if(in_array($cats, $pingCats))
+            {
+                return true;
+            }
+        }
+    }
+    else if("deny" == $pingMode)
+    {
+        foreach($categories as $cats)
+        {
+            if(in_array($cats, $pingCats))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+
---
+++ readme.txt
@@ -0,0 +1,33 @@
+=== Plugin Name ===
+Contributors: silentwind
+Donate link: http://bayu.freelancer.web.id/about/
+Tags: publish, ping.fm
+Requires at least: 2.7
+Tested up to: 2.7.1
+Stable tag: 0.4
+
+a wordpress plugin that will automatically post an update to ping.fm for every publish.
+
+== Description ==
+
+a wordpress plugin that will automatically post an update to [ping.fm](http://www.ping.fm/ "ping.fm") for every publish.
+Features:
+1. Customizable ping message
+1. Ping on all categories
+1. Ping only on selected categories
+1. Disable ping on selected categories
+
+== Installation ==
+
+1. Upload `cr-post2pingfm` directory to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Go to Settings » CR Post2Pingfm and fill in your Ping.fm [Application Key]: http://ping.fm/key/ (Ping.fm Application Key)
+
+== Frequently Asked Questions ==
+
+= Question 1 =
+...No question at the moment.
+
+== Screenshots ==
+
+1. This is where you fill in the Application Key
