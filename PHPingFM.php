c5c8c21a708eac29dc9c3e4b9fc76192c334a4d0
silentwind <silentwind@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-20 09:14:04 +0000

    fixed API_KEY
    git-svn-id: https://plugins.svn.wordpress.org/cr-post2pingfm/trunk@255155 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- PHPingFM.php
+++ PHPingFM.php
@@ -98,7 +98,7 @@ class PHPingFM {
     }
     // Setup the cURL options.
     curl_setopt_array($this->ch, array(
-      CURLOPT_POSTFIELDS => $fields + array('user_app_key' => $this->user_app_key, 'api_key' => $this->api_key,
+      CURLOPT_POSTFIELDS => $fields + array('user_app_key' => $this->user_app_key, 'api_key' => $this->api_key),
       CURLOPT_URL => 'http://api.ping.fm/v1/'. $service,
     ));
     // Load the SimpleXML.
